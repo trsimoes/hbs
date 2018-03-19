bc2ada52d04b7b74d3e0da14360b01752e901248
trsimoes <trsimoes@criticalsoftware.com>
2018-03-19 11:00:11 +0000

    Changed database. Added SQL query to fetch latest results.

--- src/main/java/pt/eden/hbs/persistence/SnapshotRepository.java
+++ src/main/java/pt/eden/hbs/persistence/SnapshotRepository.java
@@ -7,4 +7,18 @@ import pt.eden.hbs.entity.Snapshot;
  * @author : trsimoes
  */
 public interface SnapshotRepository extends CrudRepository<Snapshot, Long> {
+
+    /*
+    SELECT
+        account_balance,
+        credit_balance,
+        create_date_time
+    FROM (
+          SELECT day(create_date_time), MAX(create_date_time) as MaxTime
+          FROM SNAPSHOT
+          GROUP BY day(create_date_time)
+    ) r
+    INNER JOIN SNAPSHOT t
+    ON t.create_date_time = r.MaxTime;
+     */
 }
