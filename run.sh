7e88f4a95cd80064fface05052824f7d99a0e896
trsimoes <trsimoes@criticalsoftware.com>
2018-03-23 10:01:22 +0000

    Update run shell script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-clear && java -Dwebdriver.gecko.driver=/opt/hbs/geckodriver -jar target/hbs-1.0.1.jar
-::clear && java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9999,suspend=y -Dwebdriver.gecko.driver=/opt/hbs/geckodriver -jar target/hbs-1.0.1.jar
+clear && java -Dwebdriver.gecko.driver=/opt/hbs/geckodriver -jar hbs-1.0.1.jar
+::clear && java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9999,suspend=y -Dwebdriver.gecko.driver=/opt/hbs/geckodriver -jar hbs-1.0.1.jar
