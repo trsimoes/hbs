60e7ebdf09846adffaafc62b5db4502f899a9ced
trsimoes <trsimoes@criticalsoftware.com>
2018-03-25 17:56:03 +0100

    Updated rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,18 +1,18 @@
 echo
 echo ---------------------------
-echo Backup old HBS version
-echo ---------------------------
-echo
-mkdir -p /home/pi/backup/hbs
-tar cvf - /opt/hbs/* | gzip > /home/pi/backup/hbs/$(date +%Y%m%d%H%M%S).tar.gz
-
-echo
-echo ---------------------------
 echo Shutting down HBS Server
 echo ---------------------------
 echo
 #curl -X POST localhost:8080/shutdown
 ps -ef | grep gecko | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+
+echo
+echo ---------------------------
+echo Backup old HBS version
+echo ---------------------------
+echo
+mkdir -p /home/pi/backup/hbs
+tar cvf - /opt/hbs/* | gzip > /home/pi/backup/hbs/$(date +%Y%m%d%H%M%S).tar.gz
 
 echo
 echo ---------------------------
@@ -29,9 +29,9 @@ echo Deploying HBS Server
 echo ---------------------------
 echo
 rm -rf /opt/hbs/*
-cp target/hbs*.jar /opt/hbs/
-cp run.sh /opt/hbs/
-cp src/main/non-packaged-resources/geckodriver /opt/hbs/
+cp hbs-server/target/hbs*.jar /opt/hbs/
+cp hbs-server/run.sh /opt/hbs/
+cp drivers/geckodriver /opt/hbs/
 chmod -R 766 /opt/hbs
 ln -sf /tmp/hbs.log /opt/hbs/hbs.log
 
