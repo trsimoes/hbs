56ebad0ca35fa5ef50488cf70e56af4894f67051
trsimoes <trsimoes@criticalsoftware.com>
2018-03-25 13:27:39 +0100

    https://github.com/trsimoes/hbs/issues/9

---
+++ hbs-server/rebuild.sh
@@ -0,0 +1,40 @@
+echo.
+echo ---------------------------
+echo Backup old HBS version
+echo ---------------------------
+echo.
+mkdir -p /home/pi/backup/hbs
+tar cvf - /opt/hbs/* | gzip > /home/pi/backup/hbs/$(date +%Y%m%d%H%M%S).tar.gz
+
+echo.
+echo ---------------------------
+echo Shutting down HBS Server
+echo ---------------------------
+echo.
+curl -X POST localhost:8080/shutdown
+
+echo.
+echo ---------------------------
+echo Update and compile code
+echo ---------------------------
+echo.
+git pull
+mvn -e clean install
+
+echo.
+echo ---------------------------
+echo Deploying HBS Server
+echo ---------------------------
+echo.
+rm -rf /opt/hbs/*
+cp target/hbs*.jar /opt/hbs/
+cp run.sh /opt/hbs/
+cp src/main/non-packaged-resources/geckodriver /opt/hbs/
+chmod -R 766 /opt/hbs
+ln -sf /tmp/hbs.log /opt/hbs/hbs.log
+
+echo.
+echo ---------------------------
+echo Done
+echo ---------------------------
+echo.
