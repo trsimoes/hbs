8d1dce30279841af0c7b23c2206eb462f3bf2e1e
trsimoes <trsimoes@criticalsoftware.com>
2018-05-12 09:55:31 +0100

    Updated compiling scripts for Jenkins integration.

--- hbs-web/src/main/non-packaged-resources/run-hbs-web.sh
+++ hbs-web/src/main/non-packaged-resources/run-hbs-web.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export TERM=xterm
 cd /opt/hbs
 clear && java -jar ${build.finalName}.jar
 # clear && java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9999,suspend=y -jar ${build.finalName}.jar
