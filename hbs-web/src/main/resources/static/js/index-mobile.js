89143ca68d87afb231fe49c5172f8516be7c5af0
trsimoes <trsimoes@criticalsoftware.com>
2018-07-13 10:40:47 +0100

    Fixed javascript loading data error.

---
+++ hbs-web/src/main/resources/static/js/index-mobile.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+
+    //Widgets count
+    $('.count-to').countTo();
+
+    updateInfoBoxes();
+});
+
+function updateInfoBoxes() {
+
+
+    var latest = $.ajax({
+            url: "/latest",
+            dataType: "json",
+            async: false,
+            success: function(json) {
+                latest=json;
+            }
+        }).responseJSON;
+
+    $("div#accountBalanceDiv").countTo({from: 0, to: latest.accountBalance});
+    $("div#creditBalanceDiv").countTo({from: 0, to: latest.creditBalance});
+    $("div#euroticketBalanceDiv").countTo({from: 0, to: latest.euroticketBalance});
+    $("div#overallBalanceDiv").countTo({from: 0, to: latest.overallBalance});
+
+    var lastSnapshotDateTime = new Date(latest.createDateTime);
+    var now = new Date();
+    var diffMs = (now - lastSnapshotDateTime);
+    var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+
+    var lastUpdate = diffMins + " min ago";
+
+    $("div#createDateBalanceDiv").text(lastUpdate);
+}
